e410cd6001a968b133ac3f571dac438c2e806cfd
nailqm <38989752+nailqm@users.noreply.github.com>
2018-05-14 07:21:29 +0200

    update Artefact

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,9 +12,13 @@ public class Main {
         modele.joueurs[2] = new Joueur(modele, 3, 3, "j3", Character.Ingenieur);
         modele.joueurs[3] = new Joueur(modele, 4, 4, "j4");
         modele.iniEvent(modele.getCellule(2, 2), Event.Feu);
+        modele.iniEvent(modele.getCellule(1,3),Event.Air);
+        modele.iniEvent(modele.getCellule(4,6),Event.Eau);
+        modele.iniEvent(modele.getCellule(5,5),Event.Terre);
         for (int i = 0; i < modele.idJoueur; i++) {
             System.out.println("Joueur: " + modele.joueurs[i].nom);
         }
+        System.out.print("===Artefacts===\nFeu:(2,2)\nTerre:(5,5)\nAir(1,3)\nEau(4,6)\n");
 
         Vue vue = new Vue(modele);
     }
